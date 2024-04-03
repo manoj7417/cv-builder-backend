c85978c1494a113132700f21ea0890b5ddd1c6ac
manoj7417 <manoj@glassfrog.design>
2024-04-03 11:31:51 +0530

    Changes in index page

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const DBConnection = require('./config/db')
 const { apiKeyAuth } = require('./middlewares/auth')
 const verifyJWT = require('./middlewares/verifyJwt')
 const UserRoute = require('./routes/UserRoute')
-// const swagger = require('@fastify/swagger')
 const cors = require('@fastify/cors')
 
 require('dotenv').config()
-
 
 const fastify = require('fastify')({
     logger: {
@@ -17,14 +15,16 @@ const fastify = require('fastify')({
 })
 
 fastify.register(cors, {
-    origin: ["http://localhost:3000","http://localhost:3001","http://localhost:3002"],
-    allowedHeaders:["GET","POST","PUT","PATCH","DELETE"]
-})
+    origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:3002"],
+    allowedHeaders: ["Content-Type", "Accept", "Authorization", "x-api-key"], // Include 'x-api-key' header
+    credentials: true
+});
+
+
 
 fastify.decorate('verifyJWT', verifyJWT)
 fastify.addHook("onRequest", apiKeyAuth)
 fastify.register(UserRoute, { prefix: '/api/user' })
-
 
 const start = async () => {
     try {
