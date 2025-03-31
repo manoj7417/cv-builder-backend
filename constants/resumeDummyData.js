b1fb73361725e269489f54653a85cafd797fe647
manoj7417 <manoj@glassfrog.design>
2025-03-31 13:12:46 +0530

    Changes in resume dummy data

--- constants/resumeDummyData.js
+++ constants/resumeDummyData.js
@@ -1,10 +1,139 @@
+// const dummyData = {
+//     basics: {
+//         name: "Jane Jones",
+//         email: "Jane.Jones@gmail.com",
+//         phone: "+9112345678",
+//         country: "CA",
+//         city: "San Francisco",
+//         jobtitle: "Business Analyst",
+//         url: {
+//             label: "",
+//             href: "",
+//         },
+//         customFields: [],
+//         picture: {
+//             url: "https://geniescareerhubbucket.lon1.cdn.digitaloceanspaces.com/GCH_template_images/hometemplateImage.png",
+//             visible: true
+//         },
+//     },
+//     sections: {
+//         summary: {
+//             name: "Profile",
+//             columns: 1,
+//             visible: true,
+//             id: "profile",
+//             content: "Seasoned IT business analyst with over 12 years in business analytics, specializing in data visualization and BI software, including Qlikview and SAS. Demonstrated success in leading major analytics dashboard projects that enhanced reporting efficiency by 30%. Expertise extends to Agile and Scrum methodologies, JIRA, SQL, ETL, and Master Data Management. Proven ability in boosting operational efficiencies and achieving substantial cost savings through strategic data integration projects.",
+//         },
+//         education: {
+//             name: "Education",
+//             columns: 1,
+//             visible: true,
+//             id: "education",
+//             items: [
+//                 {
+//                     startDate: "Sep-2024",
+//                     endDate: "Dec-2024",
+//                     city: "San Francisco, CA",
+//                     description: "",
+//                     institute: "University of San Francisco",
+//                     degree: "Master of Science in Information Systems"
+//                 },
+//                 {
+//                     startDate: "Sep-2024",
+//                     endDate: "Dec-2024",
+//                     city: "Berkeley, CA",
+//                     description: "",
+//                     institute: "University of California, Berkeley",
+//                     degree: "Bachelor of Science in Computer Science"
+//                 }
+//             ],
+//         },
+//         experience: {
+//             name: "Experience",
+//             columns: 1,
+//             visible: true,
+//             id: "experience",
+//             items: [
+//                 {
+//                     jobtitle: "Senior Business Analyst",
+//                     employer: "Genentech",
+//                     startDate: "Jan-2016",
+//                     endDate: "present",
+//                     description: "",
+//                     city: "South San Francisco, CA",
+//                     highlights: ["Led the development of an advanced analytics dashboard that improved decision-making speed for senior management by 25%.", 'Facilitated over 40 workshops to define and refine project scopes, translating complex data into actionable insights for cross-functional teams.', 'Conducted in-depth data analysis to validate the feasibility of new dashboard features, which increased user engagement by 15%.', 'Crafted and documented comprehensive data metrics and business rules, significantly enhancing report accuracy and reliability.', 'Coordinated user acceptance testing, resulting in a 10% decrease in post-deployment issues.', 'Provided expert training and support to the operations team, boosting their productivity by 20% in managing production issues.']
+//                 },
+//                 {
+//                     jobtitle: "Business Systems Analyst",
+//                     employer: "Amgen",
+//                     startDate: "Jun-2012",
+//                     endDate: "Dec-2015",
+//                     description: "",
+//                     city: "Thousand Oaks, CA",
+//                     highlights: ["Implemented a strategic data integration solution that streamlined operations and saved the company $200K annually.", 'Managed a portfolio of data analytics projects, ensuring alignment with business goals and continuous delivery of value.', 'Developed user stories and use cases for BI solutions, improving data-driven decision-making across the organization.', 'Played a key role in the migration of analytics platforms to a more robust system, increasing data processing speed by 30%.', 'Led the documentation efforts for system requirements using JIRA, enhancing team productivity and project tracking.']
+//                 },
+//                 {
+//                     jobtitle: "Data Analyst",
+//                     employer: "BioMarin Pharmaceutical",
+//                     startDate: "Mar-2008",
+//                     endDate: "May-2012",
+//                     description: "",
+//                     city: "San Rafael, CA",
+//                     highlights: ["Analyzed and interpreted complex data sets to assist with strategic decision-making, influencing key business initiatives.", 'Optimized data collection and analysis processes, improving data quality and reducing time-to-insight by 20%.', 'Contributed to the development of a predictive analytics model that enhanced forecasting accuracy.', 'Supported senior analysts in creating detailed reports and presentations for stakeholders.']
+//                 }
+//             ],
+//         },
+//         projects: {
+//             name: "Projects",
+//             columns: 1,
+//             visible: true,
+//             id: "projects",
+//             items: [
+//                 {
+//                     title: "Analytics Dashboard Enhancement Project",
+//                     subtitle: "",
+//                     startDate: "Jan-2024",
+//                     endDate: "Jan-2024",
+//                     description: "<p>This initiative resulted in a 30% improvement in reporting efficiency, allowing for quicker and more accurate decision-making across the organization.</p>"
+//                 }
+//             ],
+//         },
+//         skills: {
+//             name: "Skills",
+//             columns: 1,
+//             visible: true,
+//             id: "skills",
+//             items: [
+//                 {
+//                     name: "Data Visualization",
+//                     level: "Expert"
+//                 }, {
+//                     name: 'Agile and Scrum',
+//                     level: "Expert"
+//                 }, {
+//                     name: 'JIRA',
+//                     level: "Expert"
+//                 }, {
+//                     name: "SQL",
+//                     level: "Expert"
+//                 }, {
+//                     name: 'Business Intelligence',
+//                     level: "Expert"
+//                 }
+//             ],
+//         }
+//     }
+// }
+
+
+
 const dummyData = {
     basics: {
-        name: "Jane Jones",
-        email: "Jane.Jones@gmail.com",
+        name: "Rahul Sharma",
+        email: "rahulsharma@gmail.com",
         phone: "+9112345678",
-        country: "CA",
-        city: "San Francisco",
+        country: "India",
+        city: "Pune",
         jobtitle: "Business Analyst",
         url: {
             label: "",
@@ -12,7 +141,7 @@ const dummyData = {
         },
         customFields: [],
         picture: {
-            url: "https://geniescareerhubbucket.lon1.cdn.digitaloceanspaces.com/GCH_template_images/hometemplateImage.png",
+            url: "https://geniescareerhubbucket.lon1.cdn.digitaloceanspaces.com/profile-pic-X.jpg",
             visible: true
         },
     },
@@ -22,7 +151,7 @@ const dummyData = {
             columns: 1,
             visible: true,
             id: "profile",
-            content: "Seasoned IT business analyst with over 12 years in business analytics, specializing in data visualization and BI software, including Qlikview and SAS. Demonstrated success in leading major analytics dashboard projects that enhanced reporting efficiency by 30%. Expertise extends to Agile and Scrum methodologies, JIRA, SQL, ETL, and Master Data Management. Proven ability in boosting operational efficiencies and achieving substantial cost savings through strategic data integration projects.",
+            content: "Seasoned IT business analyst with over 12 years in business analytics, specializing in data visualization and BI software, including Tableau and Power BI. Demonstrated success in leading major analytics dashboard projects that enhanced reporting efficiency by 35%. Expertise extends to Agile and Scrum methodologies, JIRA, SQL, ETL, and Master Data Management. Proven ability in boosting operational efficiencies and achieving substantial cost savings through strategic data integration projects in Indian and global markets.",
         },
         education: {
             name: "Education",
@@ -31,20 +160,20 @@ const dummyData = {
             id: "education",
             items: [
                 {
-                    startDate: "Sep-2024",
-                    endDate: "Dec-2024",
-                    city: "San Francisco, CA",
+                    startDate: "Jul-2014",
+                    endDate: "May-2016",
+                    city: "Bangalore, India",
                     description: "",
-                    institute: "University of San Francisco",
-                    degree: "Master of Science in Information Systems"
+                    institute: "Indian Institute of Management Bangalore",
+                    degree: "Post Graduate Diploma in Business Analytics"
                 },
                 {
-                    startDate: "Sep-2024",
-                    endDate: "Dec-2024",
-                    city: "Berkeley, CA",
+                    startDate: "Aug-2010",
+                    endDate: "May-2013",
+                    city: "Delhi, India",
                     description: "",
-                    institute: "University of California, Berkeley",
-                    degree: "Bachelor of Science in Computer Science"
+                    institute: "Delhi Technological University",
+                    degree: "Bachelor of Technology in Computer Science"
                 }
             ],
         },
@@ -56,30 +185,30 @@ const dummyData = {
             items: [
                 {
                     jobtitle: "Senior Business Analyst",
-                    employer: "Genentech",
+                    employer: "Tata Consultancy Services",
                     startDate: "Jan-2016",
                     endDate: "present",
                     description: "",
-                    city: "South San Francisco, CA",
-                    highlights: ["Led the development of an advanced analytics dashboard that improved decision-making speed for senior management by 25%.", 'Facilitated over 40 workshops to define and refine project scopes, translating complex data into actionable insights for cross-functional teams.', 'Conducted in-depth data analysis to validate the feasibility of new dashboard features, which increased user engagement by 15%.', 'Crafted and documented comprehensive data metrics and business rules, significantly enhancing report accuracy and reliability.', 'Coordinated user acceptance testing, resulting in a 10% decrease in post-deployment issues.', 'Provided expert training and support to the operations team, boosting their productivity by 20% in managing production issues.']
+                    city: "Mumbai, India",
+                    highlights: ["Led the development of an advanced analytics dashboard for a Fortune 500 client that improved decision-making speed by 30%", 'Facilitated over 50 workshops with stakeholders across India, US and Europe to define project requirements', 'Implemented data visualization solutions that increased client operational efficiency by 25%', 'Mentored a team of 5 junior analysts in Agile methodologies and BI tools', 'Recognized with "Star Performer" award twice for exceptional project delivery']
                 },
                 {
                     jobtitle: "Business Systems Analyst",
-                    employer: "Amgen",
+                    employer: "Infosys",
                     startDate: "Jun-2012",
                     endDate: "Dec-2015",
                     description: "",
-                    city: "Thousand Oaks, CA",
-                    highlights: ["Implemented a strategic data integration solution that streamlined operations and saved the company $200K annually.", 'Managed a portfolio of data analytics projects, ensuring alignment with business goals and continuous delivery of value.', 'Developed user stories and use cases for BI solutions, improving data-driven decision-making across the organization.', 'Played a key role in the migration of analytics platforms to a more robust system, increasing data processing speed by 30%.', 'Led the documentation efforts for system requirements using JIRA, enhancing team productivity and project tracking.']
+                    city: "Pune, India",
+                    highlights: ["Developed a data integration solution for a banking client that reduced reporting time by 40%", 'Managed analytics projects for clients in BFSI domain', 'Created dashboards that helped clients identify ₹50M in cost savings opportunities', 'Trained 20+ team members on SQL and data visualization best practices']
                 },
                 {
                     jobtitle: "Data Analyst",
-                    employer: "BioMarin Pharmaceutical",
-                    startDate: "Mar-2008",
+                    employer: "Wipro",
+                    startDate: "Jul-2008",
                     endDate: "May-2012",
                     description: "",
-                    city: "San Rafael, CA",
-                    highlights: ["Analyzed and interpreted complex data sets to assist with strategic decision-making, influencing key business initiatives.", 'Optimized data collection and analysis processes, improving data quality and reducing time-to-insight by 20%.', 'Contributed to the development of a predictive analytics model that enhanced forecasting accuracy.', 'Supported senior analysts in creating detailed reports and presentations for stakeholders.']
+                    city: "Bangalore, India",
+                    highlights: ["Developed predictive models for retail clients that improved sales forecasting accuracy by 15%", 'Automated monthly reporting processes saving 200+ man-hours annually', 'Created data validation rules that improved data quality by 20%', 'Supported clients across India and Middle East regions']
                 }
             ],
         },
@@ -90,11 +219,11 @@ const dummyData = {
             id: "projects",
             items: [
                 {
-                    title: "Analytics Dashboard Enhancement Project",
+                    title: "Unified Banking Analytics Platform",
                     subtitle: "",
                     startDate: "Jan-2024",
                     endDate: "Jan-2024",
-                    description: "<p>This initiative resulted in a 30% improvement in reporting efficiency, allowing for quicker and more accurate decision-making across the organization.</p>"
+                    description: "<p>Developed an integrated analytics solution for a leading Indian bank that consolidated data from 15+ sources, improving reporting efficiency by 35%.</p>"
                 }
             ],
         },
@@ -119,8 +248,35 @@ const dummyData = {
                 }, {
                     name: 'Business Intelligence',
                     level: "Expert"
+                }, {
+                    name: 'Python',
+                    level: "Intermediate"
                 }
             ],
+        }
+    },
+    metadata: {
+        template: "Template20",
+        page: {
+            format: {
+                type: "a4"
+            },
+        },
+        theme: {
+            background: "#ffffff",
+            text: "#000000",
+            primary: "#182e42",
+        },
+        typography: {
+            font: {
+                family: "IBM Plex Sans",
+                subset: "latin",
+                variants: ["regular"],
+                size: 13.2,
+            },
+            lineHeight: 2.45,
+            hideIcons: false,
+            underlineLinks: true,
         }
     }
 }
